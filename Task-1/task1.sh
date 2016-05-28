aacaeebf4105b39ae7bd7671a3aa88c0ed30b845
Mozartron <iamgautham123@gmail.com>
2016-05-29 00:24:28 +0530

    Update task1.sh

--- Task-1/task1.sh
+++ Task-1/task1.sh
@@ -3,10 +3,14 @@ echo Enter the path
 read path
 cd $path
 mkdir task1
+cd ./task1
 for i in {1..100}
 do
-truncate -s 10K ./task1/file$i.txt
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > ./task1/file$i.txt
+truncate -s 10K file$i.txt
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > file$i.txt
+touch -d '2 days ago' file$i.txt
+sudo chmod 444 file$i.txt
+sudo chattr +i file$i.txt
 done
-sudo chmod -R 555 task1
-sudo -k
+
+
